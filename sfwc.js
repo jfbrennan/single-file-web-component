f0b56b820a755a331e762952d71b55f813e3a10d
Jordan Brennan <jordanbrennan@me.com>
2023-06-12 16:26:16 -0500

    Add getTemplateRefs

--- sfwc.js
+++ sfwc.js
@@ -23,3 +23,19 @@ export function bindTemplateEvents(element) {
   }
 }
 
+/**
+ * Finds all elements with id attribute.
+ * @param {HTMLElement} element
+ * @returns {string|HTMLElement}
+ */
+export function getTemplateRefs(element) {
+  return new Proxy({}, {
+    get(target, refName) {
+      if (!target[refName]) {
+        const ref = element.querySelector(`[id=${refName}]`);
+        target[refName] = ref instanceof HTMLTemplateElement ? ref.content.cloneNode(true) : ref;
+      }
+      return target[refName];
+    }
+  })
+}
